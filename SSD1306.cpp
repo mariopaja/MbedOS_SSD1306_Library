ab119f4e670400809a2b74b0e30890579a95c44a
mariopaja <mariopaja@hotmail.com>
2022-03-31 10:09:56 +0200

    documentation

--- SSD1306.cpp
+++ SSD1306.cpp
@@ -3,7 +3,7 @@
 *   Created on: 29/03/2022
 *   Author: Mario Paja
 *
-* 
+*
 */
 
 #include "SSD1306.h"
@@ -11,209 +11,209 @@
 #include "Font.h"
 
 
-SSD1306::SSD1306 (PinName sda, PinName scl, char displayAddress){
-    currentTextPosition = 0;
-    displayBuffer = new char[1024];
-    if (!displayBuffer) {
-        printf ("SSD1306: Framebuffer allocation failed!\r\n");
-        while(true) { }
-    }
+SSD1306::SSD1306(PinName sda, PinName scl, char displayAddress) {
+	currentTextPosition = 0;
+	displayBuffer = new char[1024];
+	if (!displayBuffer) {
+		printf("SSD1306: Framebuffer allocation failed!\r\n");
+		while (true) {}
+	}
 
-    _I2C = new I2C (sda, scl);
-    I2CAddress = displayAddress;
-    deleteI2C = false;
+	_I2C = new I2C(sda, scl);
+	I2CAddress = displayAddress;
+	deleteI2C = false;
 #ifdef SSD1306_DEBUG
-    printf ("SSD1306 debug: fb = 0x%08.8X\r\n", displayBuffer);
+	printf("SSD1306 debug: fb = 0x%08.8X\r\n", displayBuffer);
 #endif
 }
 
-SSD1306::SSD1306 (I2C &busI2C, char displayAddress){
-    _I2C = &busI2C;
-    currentTextPosition = 0;
-    displayBuffer = new char[1024];
-    if (!displayBuffer) {
-        printf ("SSD1306: Framebuffer allocation failed!\r\n");
-        while (true) { }
-    }
-    displayAddress = displayAddress;
-    deleteI2C = true;
+SSD1306::SSD1306(I2C& busI2C, char displayAddress) {
+	_I2C = &busI2C;
+	currentTextPosition = 0;
+	displayBuffer = new char[1024];
+	if (!displayBuffer) {
+		printf("SSD1306: Framebuffer allocation failed!\r\n");
+		while (true) {}
+	}
+	displayAddress = displayAddress;
+	deleteI2C = true;
 }
 
-void SSD1306::setSpeed (speedMode spd){
-    switch (spd) {
-        case Slow:
-            _I2C->frequency(100000);
-            break;
-        case Medium:
-            _I2C->frequency(400000);
-            break;
-        case Fast:
-            _I2C->frequency(1000000);
-            break;
-    }
+void SSD1306::setSpeed(speedMode spd) {
+	switch (spd) {
+	case Slow:
+		_I2C->frequency(100000);
+		break;
+	case Medium:
+		_I2C->frequency(400000);
+		break;
+	case Fast:
+		_I2C->frequency(1000000);
+		break;
+	}
 }
 
-int SSD1306::sendCommandData (char c, char c_or_d, char lastitem){
-    int res;
+int SSD1306::sendCommandData(char c, char c_or_d, char lastitem) {
+	int res;
 
-    _I2C->start();
-    res = _I2C->write(I2CAddress);
-    if (!res) goto terminate_transaction;
-    res = _I2C->write(c_or_d | lastitem);
-    if (!res) goto terminate_transaction;
-    res = _I2C->write(c);
-    if (!res) goto terminate_transaction;
+	_I2C->start();
+	res = _I2C->write(I2CAddress);
+	if (!res) goto terminate_transaction;
+	res = _I2C->write(c_or_d | lastitem);
+	if (!res) goto terminate_transaction;
+	res = _I2C->write(c);
+	if (!res) goto terminate_transaction;
 
-    terminate_transaction:
-        _I2C->stop();
-        return res;
+terminate_transaction:
+	_I2C->stop();
+	return res;
 }
 
-int SSD1306::sendCommand (char command){
-    return sendCommandData (command, SSD1306_IS_COMMAND, SSD1306_IS_LAST);
+int SSD1306::sendCommand(char command) {
+	return sendCommandData(command, SSD1306_IS_COMMAND, SSD1306_IS_LAST);
 }
 
-int SSD1306::sendData (char data){
-    return sendCommandData (data, SSD1306_IS_DATA, SSD1306_IS_LAST);
+int SSD1306::sendData(char data) {
+	return sendCommandData(data, SSD1306_IS_DATA, SSD1306_IS_LAST);
 }
 
-int SSD1306::init (void){
-    static const char comando[] = {      0x80, SSD1306_DISPLAYOFF, 
-                                         0x80, SSD1306_CHARGEPUMP, 
-                                         0x80, 0x14, 
-                                         0x80, SSD1306_MEMORYMODE, 
-                                         0x80, 0x00, 
-                                         0x80, SSD1306_SEGREMAP | 0x1,
-                                         0x80, SSD1306_COMSCANDEC, 
-                                         0x80, SSD1306_SETBRIGHTNESS, 
-                                         0x80, 0x7F, 
-                                         0x00, SSD1306_DISPLAYON
-                                  };
+int SSD1306::init(void) {
+	static const char comando[] = { 0x80, SSD1306_DISPLAYOFF,
+										 0x80, SSD1306_CHARGEPUMP,
+										 0x80, 0x14,
+										 0x80, SSD1306_MEMORYMODE,
+										 0x80, 0x00,
+										 0x80, SSD1306_SEGREMAP | 0x1,
+										 0x80, SSD1306_COMSCANDEC,
+										 0x80, SSD1306_SETBRIGHTNESS,
+										 0x80, 0x7F,
+										 0x00, SSD1306_DISPLAYON
+	};
 
-    return _I2C->write (I2CAddress, comando, sizeof comando);
+	return _I2C->write(I2CAddress, comando, sizeof comando);
 }
 
-void SSD1306::scroll (bool refresh){
-    for (int i=128; i<1024; i++) 
-        displayBuffer[i-128] = displayBuffer[i];
+void SSD1306::scroll(bool refresh) {
+	for (int i = 128; i < 1024; i++)
+		displayBuffer[i - 128] = displayBuffer[i];
 
-    for (int i=896; i<1024; i++) 
-        displayBuffer[i] = 0;
+	for (int i = 896; i < 1024; i++)
+		displayBuffer[i] = 0;
 
-    if (refresh) 
-        refreshDisplay();
+	if (refresh)
+		refreshDisplay();
 }
 
-void SSD1306::setCursor (char row, char column){
-    currentTextPosition = row*128+column*8;
+void SSD1306::setCursor(char row, char column) {
+	currentTextPosition = row * 128 + column * 8;
 }
 
-void SSD1306::printChar (char c, bool refresh){
-    int idx;
+void SSD1306::printChar(char c, bool refresh) {
+	int idx;
 
-    idx = c*8;
-    if (currentTextPosition == 1024) {
-        scroll(refresh);
-        currentTextPosition = 896;
-    }
-    for (int i=0; i<8; i++) {
-        displayBuffer[currentTextPosition] = charset[idx+i];
-        currentTextPosition++;
-    }
-    if (refresh) 
-        refreshDisplay();
+	idx = c * 8;
+	if (currentTextPosition == 1024) {
+		scroll(refresh);
+		currentTextPosition = 896;
+	}
+	for (int i = 0; i < 8; i++) {
+		displayBuffer[currentTextPosition] = charset[idx + i];
+		currentTextPosition++;
+	}
+	if (refresh)
+		refreshDisplay();
 }
 
-void SSD1306::printString (char *s, bool refresh){
-    while (*s) printChar (*s++, refresh);
+void SSD1306::printString(char* s, bool refresh) {
+	while (*s) printChar(*s++, refresh);
 }
 
-void SSD1306::printf (const char *fmt,...){
-    char *s = new char[129];
-    va_list args;
-    
-    va_start (args, fmt);
-    vsnprintf (s, 128, fmt, args);
-    printString (s, false);
-    delete[] s;
-    va_end (args);
-}
+void SSD1306::printf(const char* fmt, ...) {
+	char* s = new char[129];
+	va_list args;
 
-
-void SSD1306::refreshDisplay(void){
-    
-    sendCommand (0xb0);
-    sendCommand (SSD1306_SETLOWCOLUMN | 0x0);  // low col = 0
-    sendCommand (SSD1306_SETHIGHCOLUMN | 0x0);  // hi col = 0
-    sendCommand (SSD1306_SETSTARTLINE | 0x0); // line #0
-
-    _I2C->start();
-    _I2C->write (I2CAddress);
-    _I2C->write (0x40);
-
-    for (int i=0; i<1024; i++) 
-        _I2C->write (displayBuffer[i]);
-    
-    _I2C->stop();
+	va_start(args, fmt);
+	vsnprintf(s, 128, fmt, args);
+	printString(s, false);
+	delete[] s;
+	va_end(args);
 }
 
 
-void SSD1306::setBrightness(char brightness){
-    sendCommand (SSD1306_SETBRIGHTNESS);
-    sendCommand (brightness);
-}
+void SSD1306::refreshDisplay(void) {
 
-void SSD1306::clearScreen (){
-    for (int i=0; i<1024; i++) 
-        displayBuffer[i] = 0;
+	sendCommand(0xb0);
+	sendCommand(SSD1306_SETLOWCOLUMN | 0x0);  // low col = 0
+	sendCommand(SSD1306_SETHIGHCOLUMN | 0x0);  // hi col = 0
+	sendCommand(SSD1306_SETSTARTLINE | 0x0); // line #0
 
-    setCursor(0,0);
-    currentTextPosition = 0;
-    refreshDisplay();
-}
+	_I2C->start();
+	_I2C->write(I2CAddress);
+	_I2C->write(0x40);
 
-void SSD1306::printPixel (char x, char y, printMode mode, bool refresh){
-    x = x % 128;
-    y = y % 64;
-    
-    switch (mode) {
-        case Normal:
-            displayBuffer[(y/8)*128+x] |=  (1<<(y%8));
-            break;
-        case Inverse:
-            displayBuffer[(y/8)*128+x] &= ~(1<<(y%8));
-            break;
-        case Xor:
-            displayBuffer[(y/8)*128+x] ^=  (1<<(y%8));
-            break;
-    }
-    if (refresh)
-        refreshDisplay();
-}
+	for (int i = 0; i < 1024; i++)
+		_I2C->write(displayBuffer[i]);
 
-bool SSD1306::getPixelState (char x, char y){
-    x = x % 128;
-    y = y % 64;
-    
-    if ( displayBuffer[(y/8)*128+x] & (1<<(y%8)) )
-        return true;
-    else
-        return false;
+	_I2C->stop();
 }
 
 
-void SSD1306::sleep(){
-    sendCommand(0xAE);
+void SSD1306::setBrightness(char brightness) {
+	sendCommand(SSD1306_SETBRIGHTNESS);
+	sendCommand(brightness);
 }
 
-void SSD1306::wake(){
-    sendCommand(0xAF);
+void SSD1306::clearScreen() {
+	for (int i = 0; i < 1024; i++)
+		displayBuffer[i] = 0;
+
+	setCursor(0, 0);
+	currentTextPosition = 0;
+	refreshDisplay();
 }
 
-void SSD1306::turnOff(){
-    sendCommand(0xAE);
+void SSD1306::printPixel(char x, char y, printMode mode, bool refresh) {
+	x = x % 128;
+	y = y % 64;
+
+	switch (mode) {
+	case Normal:
+		displayBuffer[(y / 8) * 128 + x] |= (1 << (y % 8));
+		break;
+	case Inverse:
+		displayBuffer[(y / 8) * 128 + x] &= ~(1 << (y % 8));
+		break;
+	case Xor:
+		displayBuffer[(y / 8) * 128 + x] ^= (1 << (y % 8));
+		break;
+	}
+	if (refresh)
+		refreshDisplay();
 }
 
-void SSD1306::turnOn(){
-    sendCommand(0xAF);
+bool SSD1306::getPixelState(char x, char y) {
+	x = x % 128;
+	y = y % 64;
+
+	if (displayBuffer[(y / 8) * 128 + x] & (1 << (y % 8)))
+		return true;
+	else
+		return false;
+}
+
+
+void SSD1306::sleep() {
+	sendCommand(0xAE);
+}
+
+void SSD1306::wake() {
+	sendCommand(0xAF);
+}
+
+void SSD1306::turnOff() {
+	sendCommand(0xAE);
+}
+
+void SSD1306::turnOn() {
+	sendCommand(0xAF);
 }
--- SSD1306.h
+++ SSD1306.h
@@ -3,7 +3,7 @@
 *   Created on: 29/03/2022
 *   Author: Mario Paja
 *
-* 
+*
 */
 
 #ifndef MBED_SSD1306_LIBRARY
@@ -67,216 +67,231 @@
  *
  * Example of use:
  * @code
-    #include "mbed.h"
-    #include "SSD1306.h"
+	#include "mbed.h"
+	#include "SSD1306.h"
+	
+	SSD1306 display (D14, D15);
 
+	int main()
+	{
+		display.setSpeed(SSD1306::Medium);
+		display.init();
 
-    SSD1306 display (D14, D15);
+		display.clearScreen();
 
-    int main()
-    {
-        display.setSpeed(SSD1306::Medium);  
-        display.init();  
+		display.setCursor(1,2);
+		display.printf ("Hello World");
 
-        display.clearScreen(); 
+		display.refreshDisplay();
 
-        display.setCursor(1,2);             
-        display.printf ("Hello World"); 
-        
-        display.refreshDisplay();
+		unsigned char level = 255;
+		display.setBrightness(level);
 
-        unsigned char level = 255;  
-        display.setBrightness(level);
-
-        while (true) {
-            wait_us (20000);
-        }
-    }
+		while (true) {
+			wait_us (20000);
+		}
+	}
  * @endcode
  */
 
- /*!
- * @class       SSD1306
- * @abstract    One of the best space interceptor.
- * @discussion  The mythic spaceship used by the
- *              Rebellion against the Empire.
+/*!
+* @class       SSD1306
+* @abstract    One of the best space interceptor.
+* @discussion  The mythic spaceship used by the
+*              Rebellion against the Empire.
 */
-class SSD1306 {
+class SSD1306
+{
 public:
-    
+	/*!
+	 * @enum        printMode
+	 *              Select print mode
+	 * @constant    Normal The point is set on the display
+	 * @constant    Inverse The point is erased on the display
+	 * @constant    Xor Erase pixel if it is on, otherwise set it on
+	 */
+	enum printMode
+	{
+		Normal,
+		Inverse,
+		Xor
+	};
 
-    /*!
-     * @enum        printMode
-     *              Select print mode
-     * @constant    Normal The point is set on the display
-     * @constant    Inverse The point is erased on the display
-     * @constant    Xor Erase pixel if it is on, otherwise set it on
-     */
-    enum printMode
-    {
-        Normal, 
-        Inverse, 
-        Xor      
-    };
-    
-    /*!
-     * @enum        speedMode
-     *              Select I2C bus speed
-     * @constant    Slow I2C frequency is set to 100 kHz
-     * @constant    Medium I2C frequency is set to 400 kHz
-     * @constant    Fast I2C frequency is set to 1 MHz. Use it only with short connections to host
-     */
-    enum speedMode
-    {
-        Slow,  
-        Medium, 
-        Fast     
-    };
-    
-    /**
-     * Creates an instance of a SSD1306 specifying I2C pins to use
-     *
-     * @param SDA I2C data line pin
-     * @param SCL I2C clock line pin
-     */
-    SSD1306 (PinName SDA, PinName SCL, char displayAddress = 0x78);
-    
-    /**
-     * Create an instance of a SSD1306
-     *
-     * @param busI2C I2C object
-     * @param displayAddress I2C addr of SSD1306 controller
-     */
-    SSD1306 (I2C &busI2C, char displayAddress = 0x78);
-    
-    /**
-     * Set the frequency of the I2C interface
-     *
-     * @param hz The bus frequency in hertz
-     */
-    void setSpeed (speedMode spd);
-    
-    /**
-     * Initialize the SSD1306 display
-     */
-    int init (void);
-    
-    /**
-     * Scroll up, one text line
-     *
-     * @param refresh (Optional) Refresh Display
-     */
-    void scroll (bool refresh=false);
-    
-    /**
-     * Print a character
-     *
-     * @param c ASCII code of the character to print. FOr more info visit https://www.ascii-codes.com/
-     * @param refresh (Optional) Refresh Display
-     */
-    void printChar (char c, bool refresh=false);
-    
-        
-    /**
-     * Print formated string
-     *
-     * @param fmt constant pointer string
-     */
-    void printf (const char *fmt,...);
-    
-    /**
-     * Set printing cursor
-     *
-     * @param row Expected number 0-7
-     * @param column Expected number 0-15
-     */
-    void setCursor (char row, char column);
-    
-    /**
-     * Refresh display. 
-     * Send data from memory to display
-     */
-    void refreshDisplay (void);    
-    
-    /**
-     * Set display brightness
-     *
-     * @param brightnessLevel Expected number (0-255)
-     */
-    void setBrightness (char brightnessLevel);
-    
-    /**
-     * Clear screen from all text
-     * Resets cursor to (0,0)
-     */
-    void clearScreen ();
-    
-    /**
-     * Prints one pixel.
-     * @param x x Coordinate (0-127) 
-     * @param y y Coordinate (0-63)
-     * @param mode Select print mode, otherwise Normal
-     * @param refresh (Optional) Refresh Display
-     */
-    void printPixel (char x, char y, printMode mode=Normal, bool refresh=false);
-    
-    /**
-     * Returns pixel state
-     * @param x Value (0-127)
-     * @param y Value (0-63)
-     * @return true if the pixel was set, or false otherwise
-     */
-    bool getPixelState (char x, char y);
-    
-    /**
-     * Draw line using Bresenham Algorithm
-     *
-     * @param xStart Value (0-127)
-     * @param yStart Value (0-63)
-     * @param xEnd Value (0-127)
-     * @param yEnd Value (0-63)
-     * @param mode. Select print mode, otherwise Normal
-     * @param refresh (Optional) Refresh Display
-     */
-    void drawLine (char xStart, char yStart, char xEnd, char yEnd, printMode mode=Normal, bool refresh=false);
-    
-    /** 
-     * Turn the whole display off.
-     * Reset display configuration
-     */
-    void turnOff();
-    
-    /** Turn the whole display on.  Used during initialisation. */
-    void turnOn();
-    
-    void sleep();
-    
-    void wake();
+	
 
-    int sendCommandData (char c, char c_or_d, char lastitem);  // Sends a command or data and signal if it is not the last command/data in a list
-    
-    virtual ~SSD1306 () {
-        if (!deleteI2C)
-            delete _I2C;
-        delete displayBuffer;
-    }
+
+	/**
+	 * \brief Select I2C bus speed
+	 * \param Slow I2C frequency is set to 100 kHz
+	 * \param Medium I2C frequency is set to 400 kHz
+	 * \param Fast I2C frequency is set to 1 MHz. Use it only with short connections to host
+	 */
+	enum speedMode
+	{
+		Slow,
+		Medium,
+		Fast
+	};
+
+	/**
+	 * \brief Creates an instance of a SSD1306 specifying I2C pins to use
+	 *
+	 * \param SDA I2C data line pin
+	 * \param SCL I2C clock line pin
+	 */
+	SSD1306(PinName SDA, PinName SCL, char displayAddress = 0x78);
+
+	/**
+	 * Create an instance of a SSD1306
+	 *
+	 * @param busI2C I2C object
+	 * @param displayAddress I2C addr of SSD1306 controller
+	 */
+	SSD1306(I2C& busI2C, char displayAddress = 0x78);
+
+	/**
+	 * \brief Set the frequency of the I2C interface
+	 *
+	 * \param speedHz The bus frequency in hertz
+	 */
+	void setSpeed(speedMode speedHz);
+
+	/**
+	 * \brief Initialize the SSD1306 display
+	 */
+	int init(void);
+
+	/**
+	 * \brief Scroll up, one text line
+	 *
+	 * \param refresh (Optional) Refresh Display
+	 */
+	void scroll(bool refresh = false);
+
+	/**
+	 * \brief Print a character
+	 *
+	 * \param _char ASCII code of the character to print. For more info visit https://www.ascii-codes.com/
+	 * \param refresh (Optional) Refresh Display
+	 */
+	void printChar(char _char, bool refresh = false);
+
+
+	/**
+	 * \brief Print constant pointer string
+	 *
+	 * \param fmt constant pointer string
+	 */
+	void printf(const char* fmt, ...);
+
+	/**
+	 * \brief Set printing cursor
+	 *
+	 * \param row Expected integer value (0-7)
+	 * \param column Expected integer value (0-15)
+	 */
+	void setCursor(char row, char column);
+
+	/**
+	 * \brief Refresh display.
+	 * \brief Send data from memory to display
+	 */
+	void refreshDisplay(void);
+
+	/**
+	 * \brief Set display brightness
+	 *
+	 * \param brightnessLevel Expected integer value (0-255)
+	 */
+	void setBrightness(char brightnessLevel);
+
+	/**
+	 * \brief Clear screen from all text
+	 * \brief Resets cursor to (0,0)
+	 */
+	void clearScreen();
+
+	/**
+	 * \brief Prints one pixel.
+	 *
+	 * \param x			X Coordinate (0-127)
+	 * \param y			Y Coordinate (0-63)
+	 * \param mode		Select print mode, otherwise Normal
+	 * \param refresh	(Optional) Refresh Display
+	 */
+	void printPixel(char x, char y, printMode mode = Normal, bool refresh = false);
+
+	/**
+	 * \brief Returns pixel state
+	 *
+	 * \param x X Coordinate (0-127)
+	 * \param y Y Coordinate (0-63)
+	 * \return true If the pixel was set, or false otherwise
+	 */
+	bool getPixelState(char x, char y);
+
+	/**
+	 * Draw line using Bresenham Algorithm
+	 *
+	 * @param xStart Value (0-127)
+	 * @param yStart Value (0-63)
+	 * @param xEnd Value (0-127)
+	 * @param yEnd Value (0-63)
+	 * @param mode. Select print mode, otherwise Normal
+	 * @param refresh (Optional) Refresh Display
+	 */
+	void drawLine(char xStart, char yStart, char xEnd, char yEnd, printMode mode = Normal, bool refresh = false);
+
+	/**
+	 * Turn the whole display off.
+	 * Reset display configuration
+	 */
+	void turnOff();
+
+	/** Turn the whole display on.  Used during initialisation. */
+	void turnOn();
+
+	/**
+	 * \brief Puts display on sleep
+	 */
+	void sleep();
+
+	void wake();
+
+	/**
+	 * \brief 
+	 * \param c 
+	 * \param c_or_d 
+	 * \param lastitem 
+	 * \return 
+	 */
+	int sendCommandData(char c, char c_or_d, char lastitem);
+	// Sends a command or data and signal if it is not the last command/data in a list
+
+	virtual ~SSD1306()
+	{
+		if (!deleteI2C)
+			delete _I2C;
+		delete displayBuffer;
+	}
+
 private:
-    /**
-     * Print C string
-     *
-     * @param String C string
-     * @param refresh (Optional) Refresh Display
-     */
-    void printString (char *String, bool refresh=false);
-    
+	/**
+	 * Print C string
+	 *
+	 * @param String C string
+	 * @param refresh (Optional) Refresh Display
+	 */
+	void printString(char* String, bool refresh = false);
+
 protected:
-    I2C *_I2C;               // I2C object
-    char I2CAddress;  // I2C address of SSD1306 controller
-    char *displayBuffer;               // pointer to display buffer (1024 bytes)
-    bool deleteI2C;
-    int currentTextPosition;              // Current text position (referred to screen address memory)
-    int sendCommand (char c);   // Sends a I2C command to SSD1306
-    int sendData (char d);      // Sends I2C data to SSD1306  
+	I2C* _I2C; // I2C object
+	char I2CAddress; // I2C address of SSD1306 controller
+	char* displayBuffer; // pointer to display buffer (1024 bytes)
+	bool deleteI2C;
+	int currentTextPosition; // Current text position (referred to screen address memory)
+	int sendCommand(char c); // Sends a I2C command to SSD1306
+	int sendData(char d); // Sends I2C data to SSD1306  
 };
 
 #endif
